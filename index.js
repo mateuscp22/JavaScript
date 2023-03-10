8ed02cc354686f11fba21b1708fc7a3b69a6a3c3
Mateus CP <mateuscandicocp@gmail.com>
2023-03-10 15:45:40 -0300

    cursoJS-foor_loop

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
+for (i = 1; i <= 10; i++)
+        {
+            console.log('10 + i =', 10 + i)
+        }
 
-let airport = 'BR'
-
-switch (airport){
-    case 'MCO':
-        console.log('Orlando')
-        break
-    case 'JFK':
-        console.log('JF_Kennedy')
-        break
-    case 'SEA':
-        console.log('Seattle')
-        break
-    default:
-        console.log('Unknown')
-}
