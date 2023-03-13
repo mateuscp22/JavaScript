c28c3ab3240af76ad21afa88397ff288f09318a7
Mateus CP <mateuscandicocp@gmail.com>
2023-03-13 14:02:35 -0300

    cursoJS-for_in_loop

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-/* for (i = 1; i <= 10; i++)
-        {
-            console.log('10 + ', i ,' =', 10 + i)
-        } */
+const myCar = {
+    model: 'BMW',
+    year: 2023,
+    km: 68000
+}
 
-/* i = 1
-while( i <= 10) {
-    console.log('10 + ', i ,' =', 10 + i)
-    i++
-} */
-
-i = 1
-do {
-    console.log('10 + ', i ,' =', 10 + i)
-    i++
-} while(i <= 10)
+for (let i in myCar)
+console.log(i,':',myCar[i])
