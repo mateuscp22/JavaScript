c094241f456f63eee9293e6466aac69fb3308dd1
Mateus CP <mateuscandicocp@gmail.com>
2023-03-13 14:06:41 -0300

    cursoJS-for_of_loop

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const myCar = {
+/* const myCar = {
     model: 'BMW',
     year: 2023,
     km: 68000
 }
 
 for (let i in myCar)
-console.log(i,':',myCar[i])
+console.log(i,':',myCar[i]) */
+
+const friends = ['Marcos', 'Ana', 'Jose']
+
+for (let i of friends)
+console.log(i)
