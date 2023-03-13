3f0e38004e9561956d636dc6cbc28b4e02ec4be6
Mateus CP <mateuscandicocp@gmail.com>
2023-03-13 13:57:06 -0300

    cursoJS-do_while

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
             console.log('10 + ', i ,' =', 10 + i)
         } */
 
-i = 1
-
+/* i = 1
 while( i <= 10) {
     console.log('10 + ', i ,' =', 10 + i)
     i++
-}
+} */
+
+i = 1
+do {
+    console.log('10 + ', i ,' =', 10 + i)
+    i++
+} while(i <= 10)
