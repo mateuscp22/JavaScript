b56f19180390ca58a30cf6dbda211ace3ade21c0
Mateus CP <mateuscandicocp@gmail.com>
2023-03-14 14:30:14 -0300

    cursoJS.oq_sao_arrays

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-let i = 1
-
-while (i <= 10) {
-    if (i === 8) break
-    console.log('10 + ', i ,' =', 10 + i)
-    i++
-}
+let friends = ['Marcos', 'Silvia', 'Elisa']
+friends[1] = ('Jonas')
+console.log(friends)
