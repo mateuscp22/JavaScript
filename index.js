786a53bc370aaa5f93ca356e3d2705528fd8abd6
Mateus CP <mateuscandicocp@gmail.com>
2023-03-14 14:35:46 -0300

    cursoJS-adicionando_itens_array

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-let friends = ['Marcos', 'Silvia', 'Elisa']
-friends[1] = ('Jonas')
-console.log(friends)
+const num = [7, 8, 9,]
+
+num.push(10, 11, 12) // fim do array
+num.unshift(1, 2, 3) // inicio do array
+num.splice(3, 0, 4, 5, 6) //posição , deletar?: 1 (sim), 0(não), numeros
+
+console.log(num)
