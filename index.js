8fbce0d7da4e3cbef35c2d501c5c22633ab81b04
Mateus CP <mateuscandicocp@gmail.com>
2023-03-15 15:58:10 -0300

    cursoJS-removendo_itens_array

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const movies = [
-    { id: 1, movieName: 'Dejavy' },
-    { id: 2, movieName: 'Back to the Future' },
-    { id: 3, movieName: 'The Matrix' },
-]
+const num = [1, 2, 3, 4, 5, 6, 7, 8]
+const final0 = num.pop() // remove ultimo item na array
+const final1 = num.shift() // remove o primeiro item na array
+const final2 = num.splice(2, 1) // a partir do index "x" remova "y" item(s)
 
-console.log(movies.includes({ id: 3, movieName: 'The Matrix' }))
-
-console.log(movies.find(movie => movie.movieName == 'The Matrix'))
+console.log(num)
+console.log(final0)
+console.log(final1) 
+console.log(final2)
