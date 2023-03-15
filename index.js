4f1c13c1b0d3dd6dbc0e86d75697a37db3dec739
Mateus CP <mateuscandicocp@gmail.com>
2023-03-15 16:02:01 -0300

    cursoJS-esvaziando_array

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const num = [1, 2, 3, 4, 5, 6, 7, 8]
-const final0 = num.pop() // remove ultimo item na array
-const final1 = num.shift() // remove o primeiro item na array
-const final2 = num.splice(2, 1) // a partir do index "x" remova "y" item(s)
+let num = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// num = []
+// num.length = 0
+num.splice(0, num.length) 
 
 console.log(num)
-console.log(final0)
-console.log(final1) 
-console.log(final2)
