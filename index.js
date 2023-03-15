fbe06ca872e0a4a6def4712d76a75f4e3b7dd695
Mateus CP <mateuscandicocp@gmail.com>
2023-03-15 15:51:20 -0300

    curosJS-arrow_function_array

--- index.js
+++ index.js
@@ -6,7 +6,4 @@ const movies = [
 
 console.log(movies.includes({ id: 3, movieName: 'The Matrix' }))
 
-console.log(movies.find(function (movie) {
-    return movie.movieName == 'The Matrix'
-
-}))
+console.log(movies.find(movie => movie.movieName == 'The Matrix'))
